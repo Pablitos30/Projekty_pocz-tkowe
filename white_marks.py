b4277670d2ace9f6e718bea9eb951c81b1c48625
Pablitos30 <kurpawel95@gmail.com>
2025-09-15 19:18:46 +0200

    Add files via upload

---
+++ white_marks.py
@@ -0,0 +1,6 @@
+print("Python")
+print("\tPython")
+
+print("Języki: \nPython\nC\nJavaScript")
+
+print("Języki:\n\tPython\n\tC\n\tJavaScript")
---
+++ wielkosc_liter_w_imionach.py
@@ -0,0 +1,7 @@
+imie_osoby = "Eryk"
+print(f"Witaj, {imie_osoby.title()} ! Czy chcesz dzisiaj poznawac Pythona?")
+
+
+
+nazwa = input("Czy mógłbyś podać swoje imię ?: ") 
+print(f"Witaj, {nazwa.title().strip()} ! czy chcesz dzisiaj poznawać Pythona?")
